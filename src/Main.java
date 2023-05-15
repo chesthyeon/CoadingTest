03dbb89ae0d15c52e0c276cc2916fc5a8988f55e
chesthyeon <niw927@naver.com>
2023-05-15 23:41:23 +0900

    한수

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+/// https://www.acmicpc.net/problem/1065
+/// 한수
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("이게 뭐고 브랜치 왜 안 만들어 지누");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int n = Integer.parseInt(br.readLine());
+        if (n < 100) {
+            System.out.println(n);
+            return;
+        }
+        int ans = 99;
+        for (int i = 100; i <= n; i++) {
+            int a = i / 100;
+            int b = i % 100 / 10;
+            int c = i % 10;
+            if (a - b == b - c){
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
     }
 }
