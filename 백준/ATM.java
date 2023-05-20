81d00c83ed5da6905b938bf4474f65ae939100f6
chesthyeon <niw927@naver.com>
2023-05-20 12:38:51 +0900

    ATM 수정

--- 백준/ATM.java
+++ 백준/ATM.java
@@ -1,11 +1,12 @@
-/// https://www.acmicpc.net/problem/2941
-/// 크로아티아 알파벳
+/// https://www.acmicpc.net/problem/11399
+/// ATM
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /// 입력
         final int n = Integer.parseInt(br.readLine());
         int waitTime = 0;
         int sumTime = 0;
@@ -14,7 +15,9 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        /// 정렬
         Arrays.sort(arr);
+        /// 시간을 구히기 위한 알고리즘
         for (int i = 0; i < n; i++) {
             int time = arr[i];
             waitTime += time;
